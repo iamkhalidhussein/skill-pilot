9c508bf12d8d99269f9666fd98818ed583d393b9
khalid09dev <mdkhalidhossen@hotmail.com>
2025-08-11 00:26:51 +0600

    init: skill pilot frontend initial setup done

---
+++ Frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
+});
