19af86718d0088b3fb58d766c0c745ddd79ce749
khalid09dev <mdkhalidhossen@hotmail.com>
2025-08-11 11:20:56 +0600

    refactor: moved all hooks exports to this seperated index.ts file

---
+++ Frontend/src/hooks/index.ts
@@ -0,0 +1,8 @@
+export { default as useInitStep } from "./useInitStep"
+export { default as useIntegrityMeasures } from "./useIntegrityMeasures"
+export { default as useCountdownTimer } from "./useCountdownTimer"
+export { default as useAssessmentActions } from "./useAssessmentActions"
+export { default as useComputeScore } from "./useComputeScore"
+export { default as useAssessmentUIHelpers } from "./useAssessmentController"
+export { default as useAssessmentStates } from "./useAssessmentStates"
+export { default as useFinalizedOrProceeded } from "./useFinalizedOrProceeded";
